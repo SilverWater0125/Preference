0c53c4a5dc4fceda2bfb395eafbcf16db0874d6e
SilverWater <leeeunsoo056@naver.com>
2022-07-12 21:17:29 +0900

    first commit-login

---
+++ app/src/main/java/com/example/preference/login.java
@@ -0,0 +1,17 @@
+package com.example.preference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
